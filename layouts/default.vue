4e49fb3450d576d91cc3bff06755e7707d231933
Bruno Bevilaqua <bbbevilaqua@gmailcom>
2019-02-22 09:00:44 -0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="myNavbar">
+      <b-navbar type="dark" variant="primary" toggleable>
+        <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
+        <b-collapse is-nav id="nav_dropdown_collapse">
+          <b-navbar-nav>
+            <b-nav-item href="/">Início</b-nav-item>
+            <b-nav-item href="/deputados">Deputados</b-nav-item>
+            <b-nav-item href="/partidos">Partidos</b-nav-item>
+            <b-nav-item href="/blocos">Blocos</b-nav-item>
+            <b-nav-item href="/eventos">eventos</b-nav-item>
+            <b-nav-item href="/legislaturas">Legislaturas</b-nav-item>
+            <b-nav-item href="/proposicoes">Proposições</b-nav-item>
+            <b-nav-item href="/orgaos">Órgãos</b-nav-item>
+            <b-nav-item href="/referencias">Referências</b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-navbar-brand href="/quemsomos" right>Quem Somos</b-navbar-brand>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <b-container>
+      <b-row>
+        <b-col cols="1"></b-col>
+        <b-col>
+          <nuxt />
+        </b-col>
+        <b-col cols="1"></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<style>
+.myNavbar{
+  margin-bottom: 2vh;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="text_centered">
+    <h1 class="title">
+      camaradeputadosbr - {{ user.name }}
+    </h1>
+    <h2 class="subtitle">
+      This project was maded with Nuxt.js
+    </h2>
+    <div class="links">
+      <b-button variant="success">
+        <a href="https://nuxtjs.org/" target="_blank" style="color: white;">
+          Documentation Nuxt
+        </a>
+      </b-button>
+      <b-button variant="secondary">
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" style="color: white;">
+          GitHub Nuxt
+        </a>
+      </b-button>
+    </div>
+    <h2 class="subtitle">
+      And with bootstrap-vue
+    </h2>
+    <div class="links">
+      <b-button variant="primary">
+        <a href="https://bootstrap-vue.js.org/" target="_blank" style="color: white;">
+          Documentation bootstrap-vue
+        </a>
+      </b-button>
+      <b-button variant="secondary">
+        <a href="https://github.com/bootstrap-vue/bootstrap-vue" target="_blank" style="color: white;">
+          GitHub bootstrap-vue
+        </a>
+      </b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData () {
+    return {
+      user: {
+        name: 'Bruno Bevilaqua',
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+.links{
+  margin-bottom: 1vh;
+  margin-top: 1vh;
+}
+
+.text_centered{
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+</style>
